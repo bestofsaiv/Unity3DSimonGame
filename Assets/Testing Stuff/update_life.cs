568ed9d4dffca8ff4ba1a27016f3d707ebe794d9
jasam-sheja <jasam0jasam0jasam@gmail.com>
2019-07-30 10:46:27 +0900

    other

---
+++ Assets/Testing Stuff/update_life.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class update_life : MonoBehaviour
+{
+    public GameObject partner;
+    public TextMesh _text_mesh;
+    void OnCollisionEnter(Collision info)
+    {
+        Debug.Log("collision");
+        if (info.gameObject.Equals(partner))
+            _text_mesh.text = "Not that hard";
+        else
+            Debug.Log(info.gameObject.name);
+    }
+}
