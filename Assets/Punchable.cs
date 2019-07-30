4c72e6be08c28896488ee45a46ede2de6691956a
jasam-sheja <jasam0jasam0jasam@gmail.com>
2019-07-30 10:36:57 +0900

    scripts for punching events

---
+++ Assets/Punchable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punchable : MonoBehaviour
+{
+    public string tag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPunched(Puncher puncher)
+    {
+        GameObject.Find("Log").GetComponent<InSceneLogger>().Log(tag);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
